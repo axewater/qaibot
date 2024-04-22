4622d42f0667ba6a6da612085ab4d4d048618400
Acehighness <wateraxe@gmail.com>
2024-04-22 20:36:49 +0200

    1.0 release. added version number to config.py added print version on start

--- bot/config.py
+++ bot/config.py
@@ -8,3 +8,4 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 GOOGLE_CX = os.getenv('GOOGLE_CX')
+QAI_VERSION = '1.0.0'

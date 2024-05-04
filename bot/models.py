12e610b17a348b571927546439f15283fa54b783
Acehighness <wateraxe@gmail.com>
2024-05-05 00:23:36 +0200

    new models

--- bot/models.py
+++ bot/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from .config import SQLALCHEMY_DATABASE_URI
+from sqlalchemy.orm import relationship, sessionmaker
 import datetime
+from .config import SQLALCHEMY_DATABASE_URI
 
 Base = declarative_base()
 
@@ -11,6 +11,36 @@ class BotStatistics(Base):
     id = Column(Integer, primary_key=True)
     start_time = Column(DateTime, default=datetime.datetime.utcnow)
     notes = Column(String)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    date_first_seen = Column(DateTime, default=datetime.datetime.utcnow)
+    settings = relationship("UserSetting", back_populates="user")
+    logs = relationship("Log", back_populates="user")
+
+class UserSetting(Base):
+    __tablename__ = 'user_settings'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    setting_name = Column(String)
+    setting_value = Column(String)
+    user = relationship("User", back_populates="settings")
+
+class AdminSetting(Base):
+    __tablename__ = 'admin_settings'
+    id = Column(Integer, primary_key=True)
+    setting_name = Column(String)
+    setting_value = Column(Boolean)
+
+class Log(Base):
+    __tablename__ = 'logs'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    message = Column(String)
+    user = relationship("User", back_populates="logs")
 
 # Setup the engine and session
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
